6f259dfb4d69b9875438c44b6b253d2fa032d777
muskan5guptaaa <muskan5gupta123@gmail.com>
2025-04-13 12:07:37 +0530

    jwt token

--- server/app.js
+++ server/app.js
@@ -30,27 +30,27 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-// app.get("/login", (req, res) => {
-//   const token = jwt.sign({ _id: "asdasjdhkasdasdas" }, secretKeyJWT);
+app.get("/login", (req, res) => {
+  const token = jwt.sign({ _id: "asdasjdhkasdasdas" }, secretKeyJWT);
 
-//   res
-//     .cookie("token", token, { httpOnly: true, secure: true, sameSite: "none" })
-//     .json({
-//       message: "Login Success",
-//     });
-// });
+  res
+    .cookie("token", token, { httpOnly: true, secure: true, sameSite: "none" })
+    .json({
+      message: "Login Success",
+    });
+});
 
-// io.use((socket, next) => {
-//   cookieParser()(socket.request, socket.request.res, (err) => {
-//     if (err) return next(err);
+io.use((socket, next) => {
+  cookieParser()(socket.request, socket.request.res, (err) => {
+    if (err) return next(err);
 
-//     const token = socket.request.cookies.token;
-//     if (!token) return next(new Error("Authentication Error"));
+    const token = socket.request.cookies.token;
+    if (!token) return next(new Error("Authentication Error"));
 
-//     const decoded = jwt.verify(token, secretKeyJWT);
-//     next();
-//   });
-// });
+    const decoded = jwt.verify(token, secretKeyJWT);
+    next();
+  });
+});
 
 io.on("connection", (socket) => {
   console.log("User Connected", socket.id);
